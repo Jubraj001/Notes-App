31c480b3b9eec85c8fa3863d94491fa55107f098
Jubraj001 <jubrajdev001@gmail.com>
2024-05-13 02:03:37 +0530

    feat: delete note functionality

---
+++ src/components/Notes/SingleNote.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-content">
+      <div class="content">
+        {{ note.content }}
+        <div class="has-text-right has-text-grey-light">
+          <small>{{ characterLength }} characters</small>
+        </div>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item" @click.prevent="handleDeleteClick">Delete</a>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+  import { computed } from 'vue';
+
+  const props = defineProps({
+    note: {
+      type: Object,
+      required: true
+    }
+  })
+
+  const characterLength = computed(() => {
+    return props.note.content.length;
+  });
+
+  const emit = defineEmits(['onDeleteClicked']);
+
+  const handleDeleteClick = () => {
+    emit('onDeleteClicked', props.note.id);
+  }
+</script>
