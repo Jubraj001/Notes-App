176608f52e101e1d2ece92861bbc8035ff891ace
Jubraj001 <jubrajdev001@gmail.com>
2024-05-13 01:40:54 +0530

    feat: add note functionality

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="py-4 mb-4">
-    <textarea class="textarea" placeholder="Add New Note"></textarea>
-    <button class="button is-link">Submit</button>
+    <textarea class="textarea" placeholder="Add New Note" v-model="newNote" ref="newNoteRef"></textarea>
+    <button :disabled="!newNote" class="button is-link" @click.prevent="addNote">Submit</button>
   </div>
-  <div class="card mb-4" v-for="i in 5">
+  <div class="card mb-4" v-for="note in notes" :key="note.id">
     <div class="card-content">
       <div class="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-        iaculis mauris.
+        {{ note.content }}
       </div>
     </div>
     <footer class="card-footer">
@@ -16,3 +15,27 @@
     </footer>
   </div>
 </template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const notes = ref([
+    {id: 'id1', content: 'First Note'},
+    {id: 'id2', content: 'Second Note'}
+  ]);
+
+  const newNote = ref('');
+  const newNoteRef = ref(null);
+
+  const addNote = () => {
+    const currentDate = new Date().getTime().toString();
+    const note = {
+      id: currentDate,
+      content: newNote.value
+    }
+
+    notes.value.unshift(note);
+    newNote.value = '';
+    newNoteRef.value.focus();
+  };
+</script>
